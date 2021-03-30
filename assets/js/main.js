3e78cfaf032a95c832cd1e8291741446620ebdff
igenhimel <djstar.himel@gmail.com>
2021-03-30 22:12:07 +0600

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,18 @@
-$('.navbutton').click(function () { 
-   $('#myNaV').fadeToggle('slow');
 
-  
 
+$(document).ready(function () {
+
+   $('.navbutton').click(function () { 
+      $('#myNav').fadeToggle('slow');
+   
+     
+   
+   });
+   new WOW().init();
+
+   $("#myParent").parent().addClass("cards-border");
+   $("#myParent-2").parent().addClass("cards-background");
+   $("#myParent-3").parent().addClass("cards-border");
+   $("#myParent-4").parent().addClass("cards-background");
+   
 });
-new WOW().init();
